a8a8cceae4606fac42074e0847360c52fb0d1860
thanh.ct <ctienthanhhs95@gmail.com>
2023-09-26 00:25:22 +0700

    Fix camera

--- src/screens/Scan/view.tsx
+++ src/screens/Scan/view.tsx
@@ -1,29 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text} from 'react-native';
-// import {useCameraDevices} from 'react-native-vision-camera';
-// import {Camera} from 'react-native-vision-camera';
-// import {useScanBarcodes, BarcodeFormat} from 'vision-camera-code-scanner';
+import {useCameraDevices} from 'react-native-vision-camera';
+import {Camera} from 'react-native-vision-camera';
+import {useScanBarcodes, BarcodeFormat} from 'vision-camera-code-scanner';
 import styles from './styles';
 import {AppView} from '@components';
 
 type Props = {};
 
 const View = (props: Props) => {
-  // const [hasPermission, setHasPermission] = useState(false);
-  // const devices = useCameraDevices();
-  // const device = devices.back;
+  const [hasPermission, setHasPermission] = useState(false);
+  const devices = useCameraDevices();
+  const device = devices.back;
 
-  // const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
-  //   checkInverted: true,
-  // });
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
+    checkInverted: true,
+  });
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const status = await Camera.requestCameraPermission();
-  //     setHasPermission(status === 'authorized');
-  //   })();
-  // }, []);
-  return <AppView />;
+  useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
   return (
     device != null &&
     hasPermission && (
